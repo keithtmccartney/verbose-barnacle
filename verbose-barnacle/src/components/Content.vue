017c2b87dc0f5de7a94ccf5dbb75123f8e3d85f0
Keith <keithtmccartney@hotmail.com>
2020-09-28 16:18:26 +0100

    Applying visual elements to the 'Content' component for the returning of data from the child-components 'TempVarChart' and 'Highlights'.

---
+++ verbose-barnacle/src/components/Content.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <p>The child components of Content.vue are:</p>
+        <ul>
+            <li v-for="child in childComponents">{{ child }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            childComponents: ['TempVarChart.vue', 'Highlights.vue']
+        }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    },
+}
+</script>
+
+<style>
+    
+</style>
