8af8813fb6c93e6e63a9a15d5edf32c90be5482c
Keith <keithtmccartney@hotmail.com>
2020-09-28 19:43:17 +0100

    Creating a 'weather_data' object within the 'App' component and providing a connection to the said object so as to provoke a visual display of that object (still being worked on).

--- verbose-barnacle/src/App.vue
+++ verbose-barnacle/src/App.vue
@@ -1,20 +1,41 @@
 <template>
   <div id="app">
     <p>This component's code is in {{ filename }}</p>
+    <Content v-bind:weather_data="weather_data"></Content>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      filename: 'App.vue'
+  import Content from './components/Content.vue'
+
+  export default {
+    name: 'app',
+    components: {
+      'Content': Content
+    },
+    data () {
+      return {
+        filename: 'App.vue',
+        weather_data: {
+          location: "California",
+          temperature: {
+            current: "35 C",
+          },
+          highlights: {
+            uvindex: "3",
+            windstatus: {
+              speed: "20 km/h",
+              direction: "N-E",
+            },
+            visibility: "12 km",
+          },
+        },
+      }
+    },
+    methods: {
+    },
+    computed: {
     }
-  },
-  methods: {
-  },
-  computed: {
-  }
 }
 </script>
 
