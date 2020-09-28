7dd88f5965e51b75e943e915fb2f5efe1ffc5853
Keith <keithtmccartney@hotmail.com>
2020-09-28 21:09:40 +0100

    Finalising the 'Content' component, leaving it in a broken-state (commenting-out the referenced objects will allow this to run), working on the remaining components.

--- verbose-barnacle/src/components/Content.vue
+++ verbose-barnacle/src/components/Content.vue
@@ -1,28 +1,38 @@
 <template>
-    <div>
+    <div id="pagecontent">
         <p>The child components of Content.vue are:</p>
         <ul>
             <li v-for="child in childComponents">{{ child }}</li>
         </ul>
         {{ weather_data }}
+        <temp-var-chart :tempVar="tempVar"></temp-var-chart>
+        <today-highlights :highlights="highlights"></today-highlights>
     </div>
 </template>
 
 <script>
-export default {
-    props: ["weather_data"],
-    data() {
-        return {
-            childComponents: ['TempVarChart.vue', 'Highlights.vue']
-        }
-    },
-    methods: {
+    import TempVarChart from './TempVarChar.vue'
+    import Highlights from './Highlights.vue'
+    export default {
+        props: ["weather_data"],
+        components: {
+            'temp-var-chart': TempVarChart,
+            'today-highlights': Highlights
+        },
+        data() {
+            return {
+                childComponents: ['TempVarChart.vue', 'Highlights.vue'],
+                tempVar: this.weather_data.temperature,
+                highlights: this.weather_data.highlights,
+            }
+        },
+        methods: {
 
-    },
-    computed: {
+        },
+        computed: {
 
-    },
-}
+        },
+    }
 </script>
 
 <style>
