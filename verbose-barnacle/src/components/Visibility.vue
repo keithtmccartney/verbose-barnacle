1d2ddc15f19b053b765e52196509122fc73a381f
Keith <keithtmccartney@hotmail.com>
2020-09-28 21:29:42 +0100

    Applying changes to the 'UVIndex', 'Visibility', 'WindStatus' components.

---
+++ verbose-barnacle/src/components/Visibility.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="visibility">
+        <p>Visibility: {{ visibility }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["highlights"],
+        data() {
+            return {
+                visibility: this.highlights.visibility
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+    }
+</script>
+
+<style>
+</style>
---
+++ verbose-barnacle/src/components/WindStatus.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="windstatus">
+        <p>Wind Status:</p>
+        <p>Speed - {{ speed }}; Direction - {{ direction }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: ["highlights"],
+        data() {
+            return {
+                speed: this.highlights.windstatus.speed,
+                direction: this.highlights.windstatus.direction
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        },
+    }
+</script>
+
+<style>
+</style>
